3e250a3b76dd5cec436dbc46fbc5a8b18ffcd4ba
ItzSylex <bradon162000@gmail.com>
2023-05-03 12:50:44 -0600

    debugging

--- pages/Analysis.py
+++ pages/Analysis.py
@@ -8,34 +8,34 @@ st.set_page_config(
 
 st.write('Testing')
 
-# from streamlit_extras.no_default_selectbox import selectbox
-# from app_utils.build_chart import apply_filters, get_query
+from streamlit_extras.no_default_selectbox import selectbox
+from app_utils.build_chart import apply_filters, get_query
 
-# #load custom css
-# with open('styles.css') as f:
-#     st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html = True)
+#load custom css
+with open('styles.css') as f:
+    st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html = True)
 
 
-# with st.sidebar:
+with st.sidebar:
 
-#     with st.form('filters'):
-#         PROVINCES = ['Cartago', 'San Jose', 'Guanacaste', 'Heredia', 'Alajuela', 'Limon', 'Puntarenas', 'Unknown']
+    with st.form('filters'):
+        PROVINCES = ['Cartago', 'San Jose', 'Guanacaste', 'Heredia', 'Alajuela', 'Limon', 'Puntarenas', 'Unknown']
 
-#         with st.expander('', expanded = True):
-#             year = selectbox("Year", options = [2019, 2020, 2021], label_visibility = 'visible', no_selection_label = 'All')
-#         with st.expander('', expanded = True):
-#             province = selectbox("Province", options = PROVINCES, label_visibility = 'visible', no_selection_label = 'All')
-#         with st.expander('', expanded = True):
-#             time_day = selectbox("Time of day", options = ['Early Morning', 'Morning', 'Night', 'Afternoon'], label_visibility = 'visible', no_selection_label = 'All')
-#         with st.expander('', expanded = True):
-#             gender = selectbox("Gender", options = ['Male', 'Female', 'Non applicable'], label_visibility = 'visible', no_selection_label = 'All')
+        with st.expander('', expanded = True):
+            year = selectbox("Year", options = [2019, 2020, 2021], label_visibility = 'visible', no_selection_label = 'All')
+        with st.expander('', expanded = True):
+            province = selectbox("Province", options = PROVINCES, label_visibility = 'visible', no_selection_label = 'All')
+        with st.expander('', expanded = True):
+            time_day = selectbox("Time of day", options = ['Early Morning', 'Morning', 'Night', 'Afternoon'], label_visibility = 'visible', no_selection_label = 'All')
+        with st.expander('', expanded = True):
+            gender = selectbox("Gender", options = ['Male', 'Female', 'Non applicable'], label_visibility = 'visible', no_selection_label = 'All')
 
-#         st.form_submit_button('Apply    ', type = 'primary', use_container_width= True)
+        st.form_submit_button('Apply    ', type = 'primary', use_container_width= True)
 
-# col1, col2, col3, col4 = st.columns([1.3, 2, 3, 3])
-# with col1:
-#     with st.expander('Total Crimes', expanded = True):
-#         apply_filters(get_query('TOTAL_CRIMES'), 'CRIMES_NUMBER', year, province, time_day, gender)
+col1, col2, col3, col4 = st.columns([1.3, 2, 3, 3])
+with col1:
+    with st.expander('Total Crimes', expanded = True):
+        apply_filters(get_query('TOTAL_CRIMES'), 'CRIMES_NUMBER', year, province, time_day, gender)
 #         st.caption('Total Crimes')
 #     with st.expander('Total Homicides', expanded = True):
 #         apply_filters(get_query('TOTAL_HOMICIDES'), 'CRIMES_NUMBER', year, province, time_day, gender)
