6058df4d10da0644cfa2cf3eaeed7cf6f81cc97c
ItzSylex <bradon162000@gmail.com>
2023-05-03 12:46:32 -0600

    testing

--- pages/Analysis.py
+++ pages/Analysis.py
@@ -6,43 +6,45 @@ st.set_page_config(
     layout = 'wide'
 )
 
-from streamlit_extras.no_default_selectbox import selectbox
-from app_utils.build_chart import apply_filters, get_query
+st.write('Testing')
 
-#load custom css
-with open('styles.css') as f:
-    st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html = True)
+# from streamlit_extras.no_default_selectbox import selectbox
+# from app_utils.build_chart import apply_filters, get_query
+
+# #load custom css
+# with open('styles.css') as f:
+#     st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html = True)
 
 
-with st.sidebar:
+# with st.sidebar:
 
-    with st.form('filters'):
-        PROVINCES = ['Cartago', 'San Jose', 'Guanacaste', 'Heredia', 'Alajuela', 'Limon', 'Puntarenas', 'Unknown']
+#     with st.form('filters'):
+#         PROVINCES = ['Cartago', 'San Jose', 'Guanacaste', 'Heredia', 'Alajuela', 'Limon', 'Puntarenas', 'Unknown']
 
-        with st.expander('', expanded = True):
-            year = selectbox("Year", options = [2019, 2020, 2021], label_visibility = 'visible', no_selection_label = 'All')
-        with st.expander('', expanded = True):
-            province = selectbox("Province", options = PROVINCES, label_visibility = 'visible', no_selection_label = 'All')
-        with st.expander('', expanded = True):
-            time_day = selectbox("Time of day", options = ['Early Morning', 'Morning', 'Night', 'Afternoon'], label_visibility = 'visible', no_selection_label = 'All')
-        with st.expander('', expanded = True):
-            gender = selectbox("Gender", options = ['Male', 'Female', 'Non applicable'], label_visibility = 'visible', no_selection_label = 'All')
+#         with st.expander('', expanded = True):
+#             year = selectbox("Year", options = [2019, 2020, 2021], label_visibility = 'visible', no_selection_label = 'All')
+#         with st.expander('', expanded = True):
+#             province = selectbox("Province", options = PROVINCES, label_visibility = 'visible', no_selection_label = 'All')
+#         with st.expander('', expanded = True):
+#             time_day = selectbox("Time of day", options = ['Early Morning', 'Morning', 'Night', 'Afternoon'], label_visibility = 'visible', no_selection_label = 'All')
+#         with st.expander('', expanded = True):
+#             gender = selectbox("Gender", options = ['Male', 'Female', 'Non applicable'], label_visibility = 'visible', no_selection_label = 'All')
 
-        st.form_submit_button('Apply    ', type = 'primary', use_container_width= True)
+#         st.form_submit_button('Apply    ', type = 'primary', use_container_width= True)
 
-col1, col2, col3, col4 = st.columns([1.3, 2, 3, 3])
-with col1:
-    with st.expander('Total Crimes', expanded = True):
-        apply_filters(get_query('TOTAL_CRIMES'), 'CRIMES_NUMBER', year, province, time_day, gender)
-        st.caption('Total Crimes')
-    with st.expander('Total Homicides', expanded = True):
-        apply_filters(get_query('TOTAL_HOMICIDES'), 'CRIMES_NUMBER', year, province, time_day, gender)
-        st.caption('Total lethal crimes')
+# col1, col2, col3, col4 = st.columns([1.3, 2, 3, 3])
+# with col1:
+#     with st.expander('Total Crimes', expanded = True):
+#         apply_filters(get_query('TOTAL_CRIMES'), 'CRIMES_NUMBER', year, province, time_day, gender)
+#         st.caption('Total Crimes')
+#     with st.expander('Total Homicides', expanded = True):
+#         apply_filters(get_query('TOTAL_HOMICIDES'), 'CRIMES_NUMBER', year, province, time_day, gender)
+#         st.caption('Total lethal crimes')
 
-with col2:
-    with st.expander('Distribution per years', expanded = True):
-        apply_filters(get_query('CRIMES_DISTRIBUTION_PER_YEARS'), 'CRIMES_DISTRIBUTION_PER_YEARS', year, province, time_day, gender)
-        st.caption('Total crimes per year', help = 'The year 2019 has the most amount of crimes. In the year 2020 only 67.5% of the amount of crimes in 2019 occurred, however the amount of crimes registered in 2021 are 105% of the amount of crimes in 2020, showing an increase in May 2021')
+# with col2:
+#     with st.expander('Distribution per years', expanded = True):
+#         apply_filters(get_query('CRIMES_DISTRIBUTION_PER_YEARS'), 'CRIMES_DISTRIBUTION_PER_YEARS', year, province, time_day, gender)
+#         st.caption('Total crimes per year', help = 'The year 2019 has the most amount of crimes. In the year 2020 only 67.5% of the amount of crimes in 2019 occurred, however the amount of crimes registered in 2021 are 105% of the amount of crimes in 2020, showing an increase in May 2021')
 
 # with col3:
 #     with st.expander('Crimes per victim', expanded = True):
